f86e56d41b117e9c4c1b57fb18458f655ac21d2a
Kevin Disneur <kevin@disneur.me>
2018-04-04 14:51:29 +0200

    Bump Changelog to use v0.0.2

--- formula/changelog.rb
+++ formula/changelog.rb
@@ -1,8 +1,8 @@
 class Changelog < Formula
   desc "Generate changelog based on Git history"
   homepage "https://github.com/kdisneur/changelog"
-  url "https://github.com/kdisneur/changelog/releases/download/v0.0.1/changelog-v0.0.1.tgz"
-  sha256 "64f7e9d6c9e451017a1e38a94b5e1ff862fbb2eb33953d9cb2e3e3eec5f7968c"
+  url "https://github.com/kdisneur/changelog/releases/download/v0.0.2/changelog-v0.0.2.tgz"
+  sha256 "1b853e23dd253d032255122cdcbb5b3ab6fa77a393598151a8516b8871186214"
 
   def install
     bin.install "changelog"
