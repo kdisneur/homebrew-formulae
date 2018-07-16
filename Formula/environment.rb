495e652b9d936c9b60b2dd92051ae4a77eb9a85e
Kevin Disneur <kevin@disneur.me>
2018-07-16 11:11:47 +0200

    Bump environment script

--- Formula/environment.rb
+++ Formula/environment.rb
@@ -1,8 +1,8 @@
 class Environment < Formula
   desc "Manage environment variables"
   homepage "https://github.com/kdisneur/environment"
-  url "https://github.com/kdisneur/environment/releases/download/v0.0.1/environment-v0.0.1.tgz"
-  sha256 "45a3b0b0e31db8ea74d6cb05cdd127179846dc6cbae999d6dc28ce69365c08e1"
+  url "https://github.com/kdisneur/environment/releases/download/v0.1.0/environment-v0.1.0.tgz"
+  sha256 "ab50b7ef4586810687d6a665070891d661b988aa839a1f20dd5e5d33b033d1c7"
 
   def install
     bin.install "environment"
