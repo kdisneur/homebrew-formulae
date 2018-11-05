02664c474cc01bca2f9f94200cc1f35d7951aff4
Kevin Disneur <kevin@disneur.me>
2018-11-05 14:00:45 +0100

    Fix tableformat sha

--- Formula/tableformat.rb
+++ Formula/tableformat.rb
@@ -2,7 +2,7 @@ class Tableformat < Formula
   desc "Reformat illformed tables to pretty markdown or ascii tables"
   homepage "https://github.com/kdisneur/tableformat"
   url "https://github.com/kdisneur/tableformat/releases/download/v0.0.1/tableformat-v0.0.1.tgz"
-  sha256 "3489123915938aa3cef58555637baf105f3ee1e9e24595c59779d7e45330922e"
+  sha256 "54e7754da3650e78805134438353cfd89afa7dfae69dd8fa70fc76c1b2644e89"
   def install
     bin.install "tableformat"
   end
