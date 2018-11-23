711187a88fbc4bdedeeb3d3d18cc65486217b57f
Kevin Disneur <kevin@disneur.me>
2018-11-23 21:09:52 +0100

    Release a new Changelog version

--- Formula/changelog.rb
+++ Formula/changelog.rb
@@ -1,9 +1,8 @@
 class Changelog < Formula
   desc "Generate changelog based on Git history"
   homepage "https://github.com/kdisneur/changelog"
-  url "https://github.com/kdisneur/changelog/releases/download/v0.0.2/changelog-v0.0.2.tgz"
-  sha256 "1b853e23dd253d032255122cdcbb5b3ab6fa77a393598151a8516b8871186214"
-
+  url "https://github.com/kdisneur/changelog/releases/download/v1.0.0/changelog-v1.0.0.tgz"
+  sha256 "2300f76bc0903a98ee1b36555d5be7f374e784ec6a24109dbe92ea9e48d66187"
   def install
     bin.install "changelog"
   end
