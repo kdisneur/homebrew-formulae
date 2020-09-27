d659c26f55565b67787104bacc985c25426438b5
Kevin Disneur <kevin@disneur.me>
2020-09-27 14:55:54 +0200

    b64 0.0.1

--- Formula/b64.rb
+++ Formula/b64.rb
@@ -1,9 +1,9 @@
 class B64 < Formula
   desc     "Base 64 encoder/decoder supporting URL encoding"
   homepage "https://github.com/kdisneur/b64"
-  version  "v0.0.0"
-  url      "https://github.com/kdisneur/b64/releases/download/v0.0.0/b64-darwin-amd64-v0.0.0.tgz"
-  sha256   "6af380176f3e88d80aba90ed3158a74034796abae653845f0bd316eaadddc49c"
+  version  "0.0.1"
+  url      "https://github.com/kdisneur/b64/releases/download/v0.0.1/b64-darwin-amd64-refs.tags.v0.0.1.tgz"
+  sha256   "b1a4d49ff22eba16be6d1a211b28a22a67f15beb3c1ff9e1362c3f46074934d5"
 
   def install
     bin.install "b64"
