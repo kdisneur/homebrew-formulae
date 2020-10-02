8e60d0fe3b0a3841d2e024f3ed73022eefc73b0a
Kevin Disneur <kevin@disneur.me>
2020-10-02 21:52:34 +0200

    oauth2 0.0.2

--- Formula/oauth2.rb
+++ Formula/oauth2.rb
@@ -1,9 +1,9 @@
 class Oauth2 < Formula
   desc     "OAuth2 supports authenticating using oauth2 flows"
   homepage "https://github.com/kdisneur/oauth2-cli"
-  version  "0.0.1"
-  url      "https://github.com/kdisneur/oauth2-cli/releases/download/v0.0.1/oauth2-darwin-amd64-refs.tags.v0.0.1.tgz"
-  sha256   "9f5dd7b8a5a358f45aa1b836711e8a52f317d79180a4f1e3b510e5a221d4f2b0"
+  version  "0.0.2"
+  url      "https://github.com/kdisneur/oauth2-cli/releases/download/v0.0.2/oauth2-darwin-amd64-refs.tags.v0.0.2.tgz"
+  sha256   "49bf3518cbf64b555dec9806d3017e14ed881f52838b285ac3b9cf67d98a69c8"
 
   def install
     bin.install "oauth2"
