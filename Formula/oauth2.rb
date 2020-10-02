3280f88b6f9b411d98351b4ab0e88735e5dab2d4
Kevin Disneur <kevin@disneur.me>
2020-10-02 21:45:02 +0200

    Add missing sha256 on oauth2 formula

--- Formula/oauth2.rb
+++ Formula/oauth2.rb
@@ -3,7 +3,7 @@ class Oauth2 < Formula
   homepage "https://github.com/kdisneur/oauth2-cli"
   version  "0.0.1"
   url      "https://github.com/kdisneur/oauth2-cli/releases/download/v0.0.1/oauth2-darwin-amd64-refs.tags.v0.0.1.tgz"
-  sha256   ""
+  sha256   "9f5dd7b8a5a358f45aa1b836711e8a52f317d79180a4f1e3b510e5a221d4f2b0"
 
   def install
     bin.install "oauth2"
