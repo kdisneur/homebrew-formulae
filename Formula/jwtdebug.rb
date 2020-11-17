93b6c972790c377643ec85ad4a6d6d1ca7797f20
Kevin Disneur <kevin@disneur.me>
2020-11-17 21:08:47 +0100

    jwtdebug 0.0.1

--- Formula/jwtdebug.rb
+++ Formula/jwtdebug.rb
@@ -1,8 +1,8 @@
 class Jwtdebug < Formula
   desc     "JWTDebug use a JWKSet or an actual secret to parse and verify a JWT"
   homepage "https://github.com/kdisneur/jwtdebug"
-  version  "0.0.0"
-  url      "https://github.com/kdisneur/jwtdebug/releases/download/v0.0.0/jwtdebug-darwin-amd64-refs.tags.v0.0.0.tgz"
+  version  "0.0.1"
+  url      "https://github.com/kdisneur/jwtdebug/releases/download/v0.0.1/b64-darwin-amd64-refs.tags.v0.0.1.tgz"
   sha256   ""
 
   def install
