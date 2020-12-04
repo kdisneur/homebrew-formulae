e3da6c5b811cb48da91e93fcf7ab78e7f70a5fb2
Kevin Disneur <kevin@disneur.me>
2020-12-04 22:32:52 +0100

    yamlsort 0.0.1

--- Formula/yamlsort.rb
+++ Formula/yamlsort.rb
@@ -1,8 +1,8 @@
 class Yamlsort < Formula
   desc     "yamlsort reads a YAML or partial YAML from STDIN, sort the keys and send them back to STDOUT"
   homepage "https://github.com/kdisneur/yamlsort"
-  version  "0.0.0"
-  url      "https://github.com/kdisneur/yamlsort/releases/download/v0.0.0/yamlsort-darwin-amd64-refs.tags.v0.0.0.tgz"
+  version  "0.0.1"
+  url      "https://github.com/kdisneur/yamlsort/releases/download/v0.0.1/yamlsort-darwin-amd64-refs.tags.v0.0.1.tgz"
   sha256   ""
 
   def install
