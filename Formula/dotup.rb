cf3a55db47fb95a47643a0b77aaccad6ea3145d4
Kevin Disneur <kevin@disneur.me>
2021-10-19 17:22:45 +0200

    dotup 0.0.1

--- Formula/dotup.rb
+++ Formula/dotup.rb
@@ -1,9 +1,9 @@
 class Dotup < Formula
   desc     "Dotup is in charge of maintaining a local copy of you dot files from a source folder"
   homepage "https://github.com/kdisneur/dotup"
-  version  "0.0.0"
-  url      "https://github.com/kdisneur/dotup/releases/download/v0.0.0/dotup-darwin-amd64-refs.tags.v0.0.0.tgz"
-  sha256   "a1fa4a55391603540b69dc2b4194987965ef381a8009a0ebf1a57040020fdb6b"
+  version  "0.0.1"
+  url      "https://github.com/kdisneur/dotup/releases/download/v0.0.1/dotup-darwin-amd64-refs.tags.v0.0.1.tgz"
+  sha256   "3289ea7d40ef33c77d1652b907575c19f3989f6c9907856b4759379fef69a458"
 
   def install
     bin.install "dotup"
